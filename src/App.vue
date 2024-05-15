a75a63df13073b66aac6a2e00de52716c8b3fbe2
Fajrul Aslim <fajrul.aslim@gmail.com>
2024-05-15 20:43:56 +0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Hello {{ nama }}, Selamat Datang di Vue.js
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nama: "Fajrul Aslim"
+    }
   }
+  // components: {
+    // HelloWorld
+  // }
 }
 </script>
 
